3780be01bd83af985f4d48c3946aa7be4438933a
mbpfx <mbpfx@mail.com>
2022-09-01 08:15:27 +0200

    lastupdate from laptop

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,18 +1,20 @@
 import styled from "styled-components";
 import { useState } from "react";
 const TodoListItembox = styled.li`
-  display: grid;
-  grid-template-columns: 60% 25% 15%;
+  display:grid;
+  grid-template-columns:6fr 1fr 2fr;
   width: 100%;
   margin: 0 auto;
   min-height: 4.5rem;
-  /* background-color: rgba(70, 14, 0, 0.746); */
+
+  background-color: rgba(70, 14, 0, 0.746);
   background:transparent;
 `;
 const TaskTextbox = styled.label`
-  width: calc(100% - 5px);
+  width: calc(100% - 10px);
   height: 95%;
   align-self: center;
+  justify-self:center;
   background-color: rgba(35, 35, 35, 0.746);
   border-radius:5px;
 `;
@@ -27,73 +29,33 @@ const TaskText = styled.input`
   text-transform:capitalize;
   border-radius:5px;
 `;
-const CloseItembox=styled.div`
-  display:grid;
-  align-items:center;
-  justify-items:center;
-`;
-const CloseItem=styled.a`
-  color:purple;
-`
+
 const Clockbox = styled.label`
   justify-self: center;
   width: 100%;
-  height: 100%;
+  height: calc(95% - 2px);
   align-self: center;
   display: grid;
   grid-template-columns: 1fr 1fr;
+  box-shadow:none;
+  text-shadow:none;
 `;
 const ClockContainer = styled.div`
   width: calc(100% - 2px);
-  height: calc(100% - 6px);
+  height: calc(100% - 0px);
   align-self: center;
   justify-self: center;
   display: grid;
   grid-template-columns: 3.5fr 5fr;
-  background-color: rgba(35, 35, 35, 0.746);
-  /* ff 3.6+ */
-  background: -moz-linear-gradient(
-    180deg,
-    rgba(61, 45, 0, 1) 0%,
-    rgba(154, 33, 0, 1) 52%,
-    rgba(37, 26, 0, 1) 99%
-  );
+  @media screen and (max-width:900px){
+    grid-template-columns:1fr;
+    &>*{
+      justify-self:center;
+      text-align:center;
+    }
+  }
 
-  /* safari 5.1+,chrome 10+ */
-  background: -webkit-linear-gradient(
-    180deg,
-    rgba(61, 45, 0, 1) 0%,
-    rgba(154, 33, 0, 1) 52%,
-    rgba(37, 26, 0, 1) 99%
-  );
-
-  /* opera 11.10+ */
-  background: -o-linear-gradient(
-    180deg,
-    rgba(61, 45, 0, 1) 0%,
-    rgba(154, 33, 0, 1) 52%,
-    rgba(37, 26, 0, 1) 99%
-  );
-
-  /* ie 6-9 */
-  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#251A00', endColorstr='#3D2D00', GradientType=1 );
-
-  /* ie 10+ */
-  background: -ms-linear-gradient(
-    180deg,
-    rgba(61, 45, 0, 1) 0%,
-    rgba(154, 33, 0, 1) 52%,
-    rgba(37, 26, 0, 1) 99%
-  );
-
-  /* global 94%+ browsers support */
-  background: linear-gradient(
-    180deg,
-    rgba(61, 45, 0, 1) 0%,
-    rgba(154, 33, 0, 1) 52%,
-    rgba(37, 26, 0, 1) 99%
-  );
-  background: rgba(19, 19, 19, 0.777);
+   background-color: rgba(35, 35, 35, 0.746);
   border-radius:5px;
 `;
 const ClockInput = styled.input`
@@ -101,12 +63,14 @@ const ClockInput = styled.input`
   height: 100%;
   align-self: center;
   font-size: 1.5rem;
-  color: #db0000;
+  color: #45454558;
   padding: 0.1ch;
   text-align: right;
-  background: rgba(19, 19, 19, 0.777);
-  background-color:transparent;
+
   border-radius:25px 0 0 0;
+  @media screen and (max-width:900px){
+    text-align:center;
+  }
 `;
 const ClockDurationUnit = styled.div`
   display: grid;
@@ -114,8 +78,8 @@ const ClockDurationUnit = styled.div`
   align-self: center;
   align-items: center;
   padding: 0.1ch;
-  background: rgba(19, 19, 19, 0.777);
-  background-color:transparent;
+
+
 `;
 const DurationUnitText = styled.p`
   font-size: 1.5rem;
@@ -125,13 +89,13 @@ const DurationUnitText = styled.p`
 const ClockControlsContainer = styled.div`
   display: grid;
   width: calc(100%);
-  /* grid-gap: 2px;  */
+  height: calc(100% - 0px);
+  grid-gap: 2px; 
   justify-self: center;
   align-self: center;
-  /* background-color: rgba(35, 35, 35, 0.746); */
-  /* background-color: rgba(35, 35, 35, 0.196); */
-  /* background-color: rgba(35, 35, 35, 0.196); */
-
+  border-radius:25px 0 0 25px;
+  box-shadow:none;
+  text-shadow:none;
     /* ff 3.6+ */
     background:-moz-linear-gradient(180deg, rgba(37, 18, 0, 1) 0%, rgba(0, 0, 0, 1) 51%, rgba(20, 0, 0, 1) 99%); 
 
@@ -149,10 +113,11 @@ const ClockControlsContainer = styled.div`
 
     /* global 94%+ browsers support */
     background:linear-gradient(180deg, rgba(37, 18, 0, 1) 0%, rgba(0, 0, 0, 1) 51%, rgba(20, 0, 0, 1) 99%);
+
+
     color: rgb(132, 132, 132);
   background-color: rgba(35, 35, 35, 0.196);
-  height: calc(100% - 3px);
-  border-radius:25px 0 0 25px;
+
 `;
 const ControlButton = styled.button`
   cursor: pointer;
@@ -168,7 +133,8 @@ const StartButton = styled.input`
   justify-self: center;
   align-self: center;
   width: calc(100% - 2px);
-  height: calc(100% - 6px);
+  height: calc(100% - 3px);
+  height:95%;
   cursor: pointer;
   font-size: 1.5rem;
   background-color: #232323;
@@ -182,53 +148,12 @@ export const TodoItem = () => {
   const [duration, setDuration] = useState(20);
   return (
     <TodoListItembox className="TodoListItembox">
-      <CloseItembox>
-        <CloseItem>&times;</CloseItem>
-      </CloseItembox>
 
-      <TaskTextbox htmlFor="textInput">
-        <TaskText id="textInput" type="text" placeholder="Task" />
+      <TaskTextbox htmlFor="textInput" className="textbox">
+        <TaskText id="textInput" type="text" placeholder="Task" className="text-input"/>
       </TaskTextbox>
 
-      <Clockbox htmlFor="clockInput" className="clockbox">
 
-        <ClockControlsContainer className="clock-controls">
-          <ControlButton
-            className="control-button control-plus"
-            onClick={(e) => {
-              e.preventDefault();
-              setDuration(duration + step);
-            }}
-          >
-            +
-          </ControlButton>
-          <ControlButton
-            className="control-button control-minus"
-            onClick={(e) => {
-              e.preventDefault();
-              if (duration >= step) {
-                setDuration(duration - step);
-              }
-            }}
-          >
-            -
-          </ControlButton>
-        </ClockControlsContainer>
-        
-        <ClockContainer>
-          <ClockInput
-            type="text"
-            placeholder={duration}
-            value={duration}
-            onChange={(e) => {
-              setDuration(e.target.value);
-            }}
-          ></ClockInput>
-          <ClockDurationUnit>
-            <DurationUnitText>{durationUnit}</DurationUnitText>
-          </ClockDurationUnit>
-        </ClockContainer>
-      </Clockbox>
 
       <StartButton
         type="submit"
@@ -238,6 +163,46 @@ export const TodoItem = () => {
           e.preventDefault();
         }}
       />
+
+<Clockbox htmlFor="clockInput" className="clockbox">
+
+<ClockControlsContainer className="clock-controls">
+  <ControlButton
+    className="control-button control-plus"
+    onClick={(e) => {
+      e.preventDefault();
+      setDuration(duration + step);
+    }}
+  >
+    +
+  </ControlButton>
+  <ControlButton
+    className="control-button control-minus"
+    onClick={(e) => {
+      e.preventDefault();
+      if (duration >= step) {
+        setDuration(duration - step);
+      }
+    }}
+  >
+    -
+  </ControlButton>
+</ClockControlsContainer>
+
+<ClockContainer className="clock-container">
+  <ClockInput className="clock-input"
+    type="text"
+    placeholder={duration}
+    value={duration}
+    onChange={(e) => {
+      setDuration(e.target.value);
+    }}
+  ></ClockInput>
+  <ClockDurationUnit className="clock-durationbox">
+    <DurationUnitText className="clock-durationtext">{durationUnit}</DurationUnitText>
+  </ClockDurationUnit>
+</ClockContainer>
+</Clockbox>
     </TodoListItembox>
   );
 };
